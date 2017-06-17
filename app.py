736b44812484919935b76f5940de7a3ac0b68b75
Adarsh Pathak <pathak.adarsh@gmail.com>
2017-06-17 12:39:02 +0530

    fixed minor issues

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def homepage():
+def index():
     return 'Hi'
 
-
+@app.route('/hook')
+def hook():
+    return '0'
+    
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
